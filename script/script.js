4f5cc090b7476287cc400062cc7e4feeb69cdfd9
andrey <rodomir3119841985@gmail.com>
2024-02-02 20:17:36 +0300

    button up

--- script/script.js
+++ script/script.js
@@ -1,9 +1,23 @@
+// const btn = document.querySelector('#nav-toggle');
+// const aside = document.querySelector('.sidebar');
+
+// btn.addEventListener('click',()=>{
+//   aside.classList.toggle('active');
+// });
+
+const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('#nav-toggle');
-const aside = document.querySelector('.sidebar');
+const aside = document.querySelector('#sidebar');
 
 btn.addEventListener('click',()=>{
-  aside.classList.toggle('active');
+  aside.classList.add('open');
+  overlay.classList.add('open');
 });
+overlay.addEventListener('click',()=>{
+  aside.classList.remove('open');
+  overlay.classList.remove('open');
+});
+
 
 const allButtonAcc = document.querySelectorAll('.accordion__btn');
 allButtonAcc.forEach(btn=>btn.addEventListener('click',()=>{
@@ -25,3 +39,42 @@ const openAcc = (item)=>{
   item.style.maxHeight = item.scrollHeight + 'px';
   item.style.padding = '1rem 0 1rem 0';
 }
+
+const anchors = document.querySelectorAll("a[href*='#'");
+for(let anchor of anchors){
+  anchor.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const blocID  = anchor.getAttribute('href');
+    document.querySelector(''+blocID).scrollIntoView({
+      behavior:'smooth',
+      block:'start'
+    })
+  });
+}
+
+const topButton = document.querySelector('.topButton');
+
+const btnScroll = ()=>{
+  const scrolled = window.scrollY;
+  const chord = document.documentElement.clientHeight;
+
+  if(scrolled>chord){
+    topButton.classList.add('topButton__show');
+  }
+  if(scrolled<chord){
+    topButton.classList.remove('topButton__show');
+  }
+}
+const backTop = ()=>{
+  if(window.scrollY>0){
+    window.scrollBy({
+      top: 100,
+      behavior: "smooth",
+    });
+    setTimeout(backTop, 0)
+  }
+}
+window.addEventListener('scroll', btnScroll);
+topButton.addEventListener('click', backTop);
+
+
