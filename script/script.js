5482fa3481c10a249d0771df6eee8ef98cedd063
andrey <rodomir3119841985@gmail.com>
2024-03-20 12:04:16 +0300

    site3

--- script/script.js
+++ script/script.js
@@ -6,8 +6,8 @@
 // });
 
 const overlay = document.querySelector('.overlay');
+const aside = document.querySelector('.sidebar');
 const btn = document.querySelector('#nav-toggle');
-const aside = document.querySelector('#sidebar');
 
 btn.addEventListener('click',()=>{
   aside.classList.add('open');
@@ -17,7 +17,6 @@ overlay.addEventListener('click',()=>{
   aside.classList.remove('open');
   overlay.classList.remove('open');
 });
-
 
 const allButtonAcc = document.querySelectorAll('.accordion__btn');
 allButtonAcc.forEach(btn=>btn.addEventListener('click',()=>{
@@ -40,41 +39,57 @@ const openAcc = (item)=>{
   item.style.padding = '1rem 0 1rem 0';
 }
 
-const anchors = document.querySelectorAll("a[href*='#'");
-for(let anchor of anchors){
-  anchor.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const blocID  = anchor.getAttribute('href');
-    document.querySelector(''+blocID).scrollIntoView({
-      behavior:'smooth',
-      block:'start'
+// const topButton = document.querySelector('.topButton');
+
+// const btnScroll = ()=>{
+//   const scrolled = window.scrollY;
+//   const chord = document.documentElement.clientHeight;
+
+//   if(scrolled>chord){
+//     topButton.classList.add('topButton__show');
+//   }
+//   if(scrolled<chord){
+//     topButton.classList.remove('topButton__show');
+//   }
+// }
+// const backTop = ()=>{
+//   if(window.scrollY>0){
+//     window.scrollBy({
+//       top: 100,
+//       behavior: "smooth",
+//     });
+//     setTimeout(backTop, 0)
+//   }
+// }
+// window.addEventListener('scroll', btnScroll);
+// topButton.addEventListener('click', backTop);
+
+
+const btnModal = document.querySelectorAll('[data-modal]');
+const modal = document.querySelectorAll('.modal');
+
+btnModal.forEach((item)=> {
+ item.addEventListener('click',(e)=>{
+    const target = e.currentTarget;
+    const modalAtt  = target.getAttribute('data-modal');
+    const modalId = document.getElementById(modalAtt);
+    const modalContent = modalId.querySelector('.modal__content');
+
+    modalContent.addEventListener('click',(e)=>{
+        e.stopPropagation();
     })
-  });
+    modalId.classList.add('show');
+ 
+ });
+});
+
+modal.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let clickModal = e.currentTarget;
+        closeModal(clickModal);
+    })
+})
+
+function  closeModal(clickModal) {
+    clickModal.classList.remove('show');
 }
-
-const topButton = document.querySelector('.topButton');
-
-const btnScroll = ()=>{
-  const scrolled = window.scrollY;
-  const chord = document.documentElement.clientHeight;
-
-  if(scrolled>chord){
-    topButton.classList.add('topButton__show');
-  }
-  if(scrolled<chord){
-    topButton.classList.remove('topButton__show');
-  }
-}
-const backTop = ()=>{
-  if(window.scrollY>0){
-    window.scrollBy({
-      top: 100,
-      behavior: "smooth",
-    });
-    setTimeout(backTop, 0)
-  }
-}
-window.addEventListener('scroll', btnScroll);
-topButton.addEventListener('click', backTop);
-
-
